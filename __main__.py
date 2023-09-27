405178608ee20fc4cb2d40294a042f9cef94f40c
Nathan <nathanlloyd@protonmail.com>
2023-09-27 20:38:40 +0100

    Just some formatting tweaks

--- __main__.py
+++ __main__.py
@@ -7,16 +7,16 @@ from collections import namedtuple
 from random import randint
 
 from pygame import (
-	display,
-	sprite,
-	Surface,
-	init,
-	time,
-	quit as game_quit,
-	transform,
-	image,
-	Rect,
-	event as event_queue,
+    display,
+    sprite,
+    Surface,
+    init,
+    time,
+    quit as game_quit,
+    transform,
+    image,
+    Rect,
+    event as event_queue,
 )
 from pygame.locals import QUIT
 
@@ -31,73 +31,73 @@ TILE_SIZE = 16
 
 
 class Shroom(sprite.Sprite):
-	"""Object Sprite"""
+    """Object Sprite"""
 
-	def __init__(self, image_path, origin):
-		"""Initializer"""
-		super().__init__()
-		surface = image.load(image_path).convert_alpha()
-		self.image = surface.subsurface(
-			3 * TILE_SIZE, 2 * TILE_SIZE, TILE_SIZE, TILE_SIZE
-		)
-		self.rect = Rect(origin, self.image.get_rect()[2:])
-		self.particle = ((10, 10), (1, 2), "green", 2)
+    def __init__(self, image_path, origin):
+        """Initializer"""
+        super().__init__()
+        surface = image.load(image_path).convert_alpha()
+        self.image = surface.subsurface(
+            3 * TILE_SIZE, 2 * TILE_SIZE, TILE_SIZE, TILE_SIZE
+        )
+        self.rect = Rect(origin, self.image.get_rect()[2:])
+        self.particle = ((10, 10), (1, 20), "green", 5)
 
 
 LAST_GENERATE = time.get_ticks()
+
+
 def generate(particles, sprites):
-	"""Get all sprites to emit particles"""
-	global LAST_GENERATE
-	if time.get_ticks() - LAST_GENERATE > 75:
-		for sprite in sprites:
-			particles.append(
-				Particle((sprite.rect.centerx, sprite.rect.centery), *sprite.particle)
-			)
-		LAST_GENERATE = time.get_ticks()
-		
+    """Get all sprites to emit particles"""
+    global LAST_GENERATE
+    if time.get_ticks() - LAST_GENERATE > 75:
+        for sprite in sprites:
+            particles.append(
+                Particle((sprite.rect.centerx, sprite.rect.centery), *sprite.particle)
+            )
+        LAST_GENERATE = time.get_ticks()
 
 
 def main():
-	"""main function"""
-	init()
-	screen = display.set_mode(SCREEN)
-	display.set_caption("Wolf Tracking Game")
-	game_surface = Surface(MINIMUM)
-	all_sprites = sprite.Group()
-	player = Player("assests/Wolf.png", (30, 30))
-	all_sprites.add(player)
-	for _ in range(10):
-		all_sprites.add(
-			Shroom(
-				"assests/GrassNDirt.png",
-				(randint(0, 25) * TILE_SIZE, randint(0, 25) * TILE_SIZE),
-			)
-		)
-	particles = Particles()
-	bear = Bear("assests/Bear.png", (200, 200), game_surface, particles)
-	all_sprites.add(bear)
-	clock = time.Clock()
-	last_time = time.get_ticks()
-	event_queue.pump()
-	event = event_queue.wait(1)
-	while event.type != QUIT:
-		if time.get_ticks() - last_time > 50:
-			all_sprites.update()
-			particles.collisions(game_surface.get_bounding_rect(), player)
-			generate(particles, all_sprites)
-			particles.update()
-			game_surface.fill("black")
-			all_sprites.draw(game_surface)
-			particles.draw(game_surface)
-			frame = transform.scale(game_surface, SCREEN)
-			screen.blit(frame, (0, 0))
-			display.flip()
-			last_time = time.get_ticks()
-		event_queue.pump()
-		event = event_queue.wait(1)
-	game_quit()
+    """main function"""
+    init()
+    screen = display.set_mode(SCREEN)
+    display.set_caption("Wolf Tracking Game")
+    game_surface = Surface(MINIMUM)
+    all_sprites = sprite.Group()
+    player = Player("assests/Wolf.png", (30, 30))
+    all_sprites.add(player)
+    for _ in range(10):
+        all_sprites.add(
+            Shroom(
+                "assests/GrassNDirt.png",
+                (randint(0, 25) * TILE_SIZE, randint(0, 25) * TILE_SIZE),
+            )
+        )
+    particles = Particles()
+    bear = Bear("assests/Bear.png", (200, 200), game_surface, particles)
+    all_sprites.add(bear)
+    last_time = time.get_ticks()
+    event_queue.pump()
+    event = event_queue.wait(1)
+    while event.type != QUIT:
+        if time.get_ticks() - last_time > 50:
+            all_sprites.update()
+            particles.collisions(game_surface.get_bounding_rect(), player)
+            generate(particles, all_sprites)
+            particles.update()
+            game_surface.fill("black")
+            all_sprites.draw(game_surface)
+            particles.draw(game_surface)
+            frame = transform.scale(game_surface, SCREEN)
+            screen.blit(frame, (0, 0))
+            display.flip()
+            last_time = time.get_ticks()
+        event_queue.pump()
+        event = event_queue.wait(1)
+    game_quit()
 
 
 if __name__ == "__main__":
-	main()
-	sys.exit()
+    main()
+    sys.exit()
--- characters.py
+++ characters.py
@@ -1,18 +1,18 @@
 """characters"""
-from random import choice, randint
+from random import choice
 from time import time
 
 from pygame import sprite, Rect, image, key
 from pygame.locals import (
-	K_LEFT,
-	K_RIGHT,
-	K_UP,
-	K_DOWN,
-	K_w,
-	K_a,
-	K_s,
-	K_d,
-	K_LSHIFT,
+    K_LEFT,
+    K_RIGHT,
+    K_UP,
+    K_DOWN,
+    K_w,
+    K_a,
+    K_s,
+    K_d,
+    K_LSHIFT,
 )
 
 PLAYER_SPEED = 4
@@ -34,204 +34,205 @@ UP_RUNNING = 11
 
 
 class Character(sprite.Sprite):
-	"""Player Sprite"""
+    """Player Sprite"""
 
-	def __init__(self, image_path, origin):
-		"""Initializer"""
-		super().__init__()
-		self.sprite_sheet = image.load(image_path).convert_alpha()
-		self.current_frame = 0
-		self.state = RIGHT_IDLE
-		self.run = (1, 0)
-		self.update_frames()
-		self.image = self.frames[self.current_frame]
-		self.rect = self.image.get_rect()
-		self.rect.centerx = origin[0]
-		self.rect.centery = origin[1]
-		self.last_sample = time()
+    def __init__(self, image_path, origin):
+        """Initializer"""
+        super().__init__()
+        self.sprite_sheet = image.load(image_path).convert_alpha()
+        self.current_frame = 0
+        self.state = RIGHT_IDLE
+        self.run = (1, 0)
+        self.update_frames()
+        self.image = self.frames[self.current_frame]
+        self.rect = self.image.get_rect()
+        self.rect.centerx = origin[0]
+        self.rect.centery = origin[1]
+        self.last_sample = time()
 
-	def update_frames(self):
-		"""Change what frames are being used for the animation"""
-		self.frames = []
-		for i in range(4):
-			self.frames.append(
-				self.sprite_sheet.subsurface(
-					Rect(
-						i * TILE_SIZE,
-						self.state * TILE_SIZE,
-						TILE_SIZE,
-						TILE_SIZE,
-					)
-				)
-			)
+    def update_frames(self):
+        """Change what frames are being used for the animation"""
+        self.frames = []
+        for i in range(4):
+            self.frames.append(
+                self.sprite_sheet.subsurface(
+                    Rect(
+                        i * TILE_SIZE,
+                        self.state * TILE_SIZE,
+                        TILE_SIZE,
+                        TILE_SIZE,
+                    )
+                )
+            )
 
-	def right(self):
-		"""right"""
-		self.rect.x += PLAYER_SPEED * self.run[0]
-		self.state = RIGHT_WALKING + self.run[1]
+    def right(self):
+        """right"""
+        self.rect.x += PLAYER_SPEED * self.run[0]
+        self.state = RIGHT_WALKING + self.run[1]
 
-	def left(self):
-		"""left"""
-		self.rect.x -= PLAYER_SPEED * self.run[0]
-		self.state = LEFT_WALKING + self.run[1]
+    def left(self):
+        """left"""
+        self.rect.x -= PLAYER_SPEED * self.run[0]
+        self.state = LEFT_WALKING + self.run[1]
 
-	def up(self):
-		"""up"""
-		self.rect.y -= PLAYER_SPEED * self.run[0]
-		self.state = UP_WALKING + self.run[1]
+    def up(self):
+        """up"""
+        self.rect.y -= PLAYER_SPEED * self.run[0]
+        self.state = UP_WALKING + self.run[1]
 
-	def down(self):
-		"""down"""
-		self.rect.y += PLAYER_SPEED * self.run[0]
-		self.state = DOWN_WALKING + self.run[1]
-	
-	def sample(self):
-		"""This move checks 360 around the character"""
-		self.run = (1, 0)
-		if time() - self.last_sample > .5:
-			self.state = (self.state + 1) % 4
-			self.last_sample = time()
+    def down(self):
+        """down"""
+        self.rect.y += PLAYER_SPEED * self.run[0]
+        self.state = DOWN_WALKING + self.run[1]
 
-	def idle(self):
-		"""idle"""
-		self.run = (1, 0)
-		self.state %= 4
+    def sample(self):
+        """This move checks 360 around the character"""
+        self.run = (1, 0)
+        if time() - self.last_sample > 0.5:
+            self.state = (self.state + 1) % 4
+            self.last_sample = time()
+
+    def idle(self):
+        """idle"""
+        self.run = (1, 0)
+        self.state %= 4
 
 
 class Bear(Character):
-	"""Bear Sprite"""
+    """Bear Sprite"""
 
-	def __init__(self, image_path, origin, surface, particles):
-		"""Initizializer"""
-		super().__init__(image_path, origin)
-		self.particle = ((2, 2), (2, 16), "red", 100)
-		self.last_smelt = -1
-		self.internal_clock = time()
-		self.move = choice([self.right, self.left, self.up, self.down])
-		self.current_frame = (self.current_frame + 1) % 4
-		self.image = self.frames[self.current_frame]
-		self.surface = surface
-		self.particles = particles
+    def __init__(self, image_path, origin, surface, particles):
+        """Initizializer"""
+        super().__init__(image_path, origin)
+        self.particle = ((2, 2), (2, 16), "red", 100)
+        self.last_smelt = -1
+        self.internal_clock = time()
+        self.move = choice([self.right, self.left, self.up, self.down])
+        self.current_frame = (self.current_frame + 1) % 4
+        self.image = self.frames[self.current_frame]
+        self.surface = surface
+        self.particles = particles
 
-	def update_frames(self):
-		"""Change what frames are being used for the animation"""
-		self.frames = []
-		for i in range(4):
-			self.frames.append(
-				self.sprite_sheet.subsurface(
-					Rect(
-						i * 24,
-						self.state * 24,
-						24,
-						24,
-					)
-				)
-			)
+    def update_frames(self):
+        """Change what frames are being used for the animation"""
+        self.frames = []
+        for i in range(4):
+            self.frames.append(
+                self.sprite_sheet.subsurface(
+                    Rect(
+                        i * 24,
+                        self.state * 24,
+                        24,
+                        24,
+                    )
+                )
+            )
 
-	def smells_player(self):
-		rect = Rect(self.rect)
-		if (self.state % 4) == RIGHT_IDLE:
-			rect.centerx += TILE_SIZE * 2
-		elif (self.state % 4) == LEFT_IDLE:
-			rect.centerx -= TILE_SIZE * 2
-		elif (self.state % 4) == DOWN_IDLE:
-			rect.centery += TILE_SIZE * 2
-		elif (self.state % 4) == UP_IDLE:
-			rect.centery -= TILE_SIZE * 2
-		count = 0
-		for particle in filter(lambda p: p.color not in ["red", "green"], self.particles):
-			count += 1 if particle in rect else 0
-		print(end=f"\x1b[2K I smell {count} wolf blood!\r")
-		return count
-	
-	def smells_self(self):
-		"""TODO
-		search particles infront of bear
-		"""
-		return False
+    def smells_player(self):
+        rect = Rect(self.rect)
+        if (self.state % 4) == RIGHT_IDLE:
+            rect.centerx += TILE_SIZE * 2
+        elif (self.state % 4) == LEFT_IDLE:
+            rect.centerx -= TILE_SIZE * 2
+        elif (self.state % 4) == DOWN_IDLE:
+            rect.centery += TILE_SIZE * 2
+        elif (self.state % 4) == UP_IDLE:
+            rect.centery -= TILE_SIZE * 2
+        count = 0
+        for particle in filter(
+            lambda p: p.color not in ["red", "green"], self.particles
+        ):
+            count += 1 if particle in rect else 0
+        return count
 
-	def update(self):
-		"""update"""
-		rect = self.surface.get_bounding_rect()
-		# scent check
-		if self.smells_player():
-			self.internal_clock = time()
-			self.move = self.sample
-		elif self.smells_self():
-			self.internal_clock = time()
-		# border check
-		if self.rect.top < rect.top:
-			self.move = self.down
-			self.internal_clock = time()
-		elif self.rect.bottom > rect.bottom:
-			self.move = self.up
-			self.internal_clock = time()
-		elif self.rect.left < rect.left:
-			self.move = self.right
-			self.internal_clock = time()
-		elif self.rect.right > rect.right:
-			self.move = self.left
-			self.internal_clock = time()
-		# time check
-		if time() - self.internal_clock > 3:
-			# should add a rotation feature to checks smells to either side
-			self.move = choice([self.right, self.left, self.up, self.down])
-			self.internal_clock = time()
-		self.move()
-		self.update_frames()
-		self.current_frame = (self.current_frame + 1) % 4
-		self.image = self.frames[self.current_frame]
+    def smells_self(self):
+        """TODO
+        search particles infront of bear
+        """
+        return False
+
+    def update(self):
+        """update"""
+        rect = self.surface.get_bounding_rect()
+        # scent check
+        if self.smells_player():
+            self.internal_clock = time()
+            self.move = self.sample
+        elif self.smells_self():
+            self.internal_clock = time()
+        # border check
+        if self.rect.top < rect.top:
+            self.move = self.down
+            self.internal_clock = time()
+        elif self.rect.bottom > rect.bottom:
+            self.move = self.up
+            self.internal_clock = time()
+        elif self.rect.left < rect.left:
+            self.move = self.right
+            self.internal_clock = time()
+        elif self.rect.right > rect.right:
+            self.move = self.left
+            self.internal_clock = time()
+        # time check
+        if time() - self.internal_clock > 3:
+            # should add a rotation feature to checks smells to either side
+            self.move = choice([self.right, self.left, self.up, self.down])
+            self.internal_clock = time()
+        self.move()
+        self.update_frames()
+        self.current_frame = (self.current_frame + 1) % 4
+        self.image = self.frames[self.current_frame]
 
 
 class Player(Character):
-	"""Player Sprite"""
+    """Player Sprite"""
 
-	def __init__(self, image_path, origin):
-		"""Initializer"""
-		super().__init__(image_path, origin)
-		self.particle = ((2, 2), (2, 16), "pink", 100)
-		self.cone = Rect(0, 0, TILE_SIZE*2, TILE_SIZE*2)
-		self.set_cone()
+    def __init__(self, image_path, origin):
+        """Initializer"""
+        super().__init__(image_path, origin)
+        self.particle = ((2, 2), (2, 16), "pink", 100)
+        self.cone = Rect(0, 0, TILE_SIZE * 2, TILE_SIZE * 2)
+        self.set_cone()
 
-	def set_cone(self):
-		"""set area for smelling cone"""
-		if (self.state % 4) == RIGHT_IDLE:
-			self.cone.centerx = self.rect.centerx + TILE_SIZE*2
-			self.cone.centery = self.rect.centery
-		elif (self.state % 4) == LEFT_IDLE:
-			self.cone.centerx = self.rect.centerx - TILE_SIZE*2
-			self.cone.centery = self.rect.centery
-		elif (self.state % 4) == DOWN_IDLE:
-			self.cone.centerx = self.rect.centerx
-			self.cone.centery = self.rect.centery + TILE_SIZE*2
-		elif (self.state % 4) == UP_IDLE:
-			self.cone.centerx = self.rect.centerx
-			self.cone.centery = self.rect.centery - TILE_SIZE*2
+    def set_cone(self):
+        """set area for smelling cone"""
+        if (self.state % 4) == RIGHT_IDLE:
+            self.cone.centerx = self.rect.centerx + TILE_SIZE * 2
+            self.cone.centery = self.rect.centery
+        elif (self.state % 4) == LEFT_IDLE:
+            self.cone.centerx = self.rect.centerx - TILE_SIZE * 2
+            self.cone.centery = self.rect.centery
+        elif (self.state % 4) == DOWN_IDLE:
+            self.cone.centerx = self.rect.centerx
+            self.cone.centery = self.rect.centery + TILE_SIZE * 2
+        elif (self.state % 4) == UP_IDLE:
+            self.cone.centerx = self.rect.centerx
+            self.cone.centery = self.rect.centery - TILE_SIZE * 2
 
-	def update(self):
-		"""Overwrite of Sprite updating"""
-		keys = key.get_pressed()
-		if any(keys):
-			if keys[K_LSHIFT]:
-				self.run = (2, 4)
-			if keys[K_RIGHT] or keys[K_d]:
-				self.right()
-			elif keys[K_LEFT] or keys[K_a]:
-				self.left()
-			elif keys[K_DOWN] or keys[K_s]:
-				self.down()
-			elif keys[K_UP] or keys[K_w]:
-				self.up()
-			else:
-				self.idle()
-			self.update_frames()
-			self.set_cone()
-		elif self.state >= 8:
-			self.run = (1, 0)
-			self.state = max(0, self.state - 8)
-			self.update_frames()
-		elif self.state >= 4:
-			self.state = max(0, self.state - 4)
-			self.update_frames()
-		self.current_frame = (self.current_frame + 1) % 4
-		self.image = self.frames[self.current_frame]
+    def update(self):
+        """Overwrite of Sprite updating"""
+        keys = key.get_pressed()
+        if any(keys):
+            if keys[K_LSHIFT]:
+                self.run = (2, 4)
+            if keys[K_RIGHT] or keys[K_d]:
+                self.right()
+            elif keys[K_LEFT] or keys[K_a]:
+                self.left()
+            elif keys[K_DOWN] or keys[K_s]:
+                self.down()
+            elif keys[K_UP] or keys[K_w]:
+                self.up()
+            else:
+                self.idle()
+            self.update_frames()
+            self.set_cone()
+        elif self.state >= 8:
+            self.run = (1, 0)
+            self.state = max(0, self.state - 8)
+            self.update_frames()
+        elif self.state >= 4:
+            self.state = max(0, self.state - 4)
+            self.update_frames()
+        self.current_frame = (self.current_frame + 1) % 4
+        self.image = self.frames[self.current_frame]
